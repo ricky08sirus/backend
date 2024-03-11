49612fae1b6dc005e670263f0d8953ec2026b60a
aman <aman08jul@gmail.com>
2024-03-11 22:52:28 +0530

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = 4000
+
+
+app.get('/',(req,res)=>{
+    res.send('hello world');
+
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`example app listening on ${port}`);
+
+})
+
